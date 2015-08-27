2832b4e5e707b50fec81054b1a039b5a815c2908
grayswan <swan.kristen@gmail.com>
2015-08-27 10:48:45 -0400

    problem 2

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 var count = items.length;
-console.log(count);
 
 var sum = 0;
 items.forEach(function(item) {
   sum += item.price;
 });
-console.log(sum);
 var avg = (sum/count).toFixed(2);
-console.log("The average price is" + '' avg);
+console.log("The average price is $" + avg);
+
+
+var itemsInRange = []
+items.forEach(function(item) {
+  if(item.price >= 14 && item.price <= 18)
+    itemsInRange.push(item);
+});
+console.log("Items that cost between $14.00 USD and $18.00 USD:", itemsInRange);
